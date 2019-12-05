0a9ff33062a145d5372948b5965848608475e749
maixiong29 <54956325+maixiong29@users.noreply.github.com>
2019-12-04 22:57:36 -0600

    initial

---
+++ homework11/Develop/README.md
@@ -0,0 +1,8 @@
+Note Taker EXPRESS
+
+This note taker helps a user create, save, and delete notes that the user would like to jot down whenever needed.  The notes are saved on the server and will not get deleted even when the user exits out of the application.  This will help the user save time and is conveient for the user to use at anytime.
+
+*******
+
+
+How the application works is, the User will type in a title of the note and then in the section below, the user will type in the note that the user would like to save.  Once done, the user can click on the "floppy disk" icon to save the note and it will appear on the left side of the page.  The user can click on the list of saved notes and see what the notes are (displayed once clicked on).  The user can also delte the note by cicked on the "trash bin" icon and it will be deleted from the list.
---
+++ homework11/Develop/server.js
@@ -0,0 +1,69 @@
+var express = require("express");
+var path = require("path");
+var app = express();
+var fs = require("fs");
+var PORT = process.env.PORT || 8080;
+var notes;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'))
+
+var savedNotes = fs.readFileSync("./db/db.json", "UTF-8");
+if (savedNotes) {
+    var oldNotes = JSON.parse(savedNotes);
+    notes = oldNotes;
+} else {
+    notes = [];
+}
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/api/notes", function (req, res) {
+    return res.json(notes);
+});
+
+app.post("/api/notes", function (req, res) {
+    var newNote = req.body;
+    console.log(newNote);
+    notes.push(newNote);
+    res.json(newNote);
+    assignID();
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes, null, 2), function (err) {
+        if (err) 
+            throw err
+    });
+});
+
+app.delete("/api/notes/:id", function(req, res) {
+    console.log(req);
+    const targetNote = parseInt(req.body.id);
+    notes.splice(targetNote, 1);
+    for(let i = 0; i < notes.length; i++){
+      notes[i].id = i;
+    }
+    let stringifiedNotes = JSON.stringify(notes);
+    fs.writeFile("db/db.json", stringifiedNotes, function(err) {
+      if(err) {
+          return console.log(err);
+      }
+      console.log("The file was deleted!");
+    });
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+  });
+
+function assignID() {
+    for (i = 0; i < notes.length; i ++) {
+        notes[i].id = i;
+    }
+}
+
+app.listen(PORT, function () {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
