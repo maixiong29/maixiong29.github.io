c9fd5876dd135ae0c153485049aceb801c0f9409
maixiong29 <54956325+maixiong29@users.noreply.github.com>
2020-01-29 22:40:26 -0600

    initial

---
+++ homework16/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3001;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+// Requiring our routes
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
